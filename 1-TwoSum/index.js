8d7e03d1128f46a947171a805859a9974ed85258
Fritiof Rusck <fritiof@rusck.se>
2024-09-05 19:39:15 +0200

    feat: 1-TwoSum
    feat: 1-TwoSum

---
+++ 1-TwoSum/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const seen = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (seen.has(diff)) {
+      return [seen.get(diff), i];
+    }
+    seen.set(nums[i], i);
+  }
+};
