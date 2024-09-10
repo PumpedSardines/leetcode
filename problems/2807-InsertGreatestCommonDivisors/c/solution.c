dd093b4ca8dab7618999fe9cd79ff53814da932c
Fritiof Rusck <fritiof@rusck.se>
2024-09-10 10:09:37 +0200

    feat(2807): implement in c

---
+++ problems/2807-InsertGreatestCommonDivisors/c/solution.c
@@ -0,0 +1,34 @@
+int get_gcd(int a, int b) {
+  if (!b) {
+    return a;
+  }
+
+  return get_gcd(b, a % b);
+}
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* insertGreatestCommonDivisors(struct ListNode* head){
+  struct ListNode* cursor = head;
+
+  int next_node_block_index = 0;
+  struct ListNode* next_node_blocks = (struct ListNode*)malloc(sizeof(struct ListNode) * 5001);
+
+  while(cursor->next) {
+    int gcd = get_gcd(cursor->val, cursor->next->val);
+    struct ListNode* next_node = &next_node_blocks[next_node_block_index];
+    next_node_block_index++;
+    next_node->val = gcd;
+    next_node->next = cursor->next;
+    cursor->next = next_node;
+    cursor = next_node->next;
+  }
+
+  return head;
+}
+
