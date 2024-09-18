24989d45d8bc9620a4949ef73b15fa08ef58700d
Fritiof Rusck <fritiof@rusck.se>
2024-09-18 20:36:07 +0200

    feat(codegolf): update sudoku solution

--- codegolf/problems/sudoku/index.js
+++ codegolf/problems/sudoku/index.js
@@ -6,6 +6,6 @@ let j=o(x,y)
 if(k==81)return e
 if(e[j]!=" ")return s(k+1)
 let p={}
-for(i=0;i<9;i++)for(r of [o(i,y),o(x,i),o(~~(x/3)*3+i%3,~~(y/3)*3+~~(i/3))])p[e[r]]=1
+for(i=0;i<9;i++)[o(i,y),o(x,i),o(~~(x/3)*3+i%3,~~(y/3)*3+~~(i/3))].map(r=>p[e[r]]=1)
 for(let i=1;i<=9;i++)if(!p[i]&&s(k+1,e[j]=i))return e
 e[j]=" "})(0).join(""))
