2ebaaae9e99bbc98795b632007735b2fdafd2ee0
Fritiof Rusck <fritiof@rusck.se>
2024-09-20 14:40:49 +0200

    feat(helpers): add caching to daily leetcode fetcher

--- .helpers/fetch-daily-leetcode/index.js
+++ .helpers/fetch-daily-leetcode/index.js
@@ -2,7 +2,37 @@ import { LeetCode, fetcher } from "leetcode-query";
 import { chromium } from "playwright-extra";
 import stealth from "puppeteer-extra-plugin-stealth";
 
-// setup browser
+import fs from "fs/promises";
+import os from "os";
+import path from "path";
+
+const tempDir = os.tmpdir();
+const leetCodeTempDir = path.join(tempDir, "leetcode-daily");
+
+await fs.mkdir(leetCodeTempDir).catch(() => {});
+const cache = JSON.parse(
+  await fs.readFile(path.join(leetCodeTempDir, "cache.json")).catch(() => "{}"),
+);
+const todayKey = new Date().toISOString().split("T")[0];
+
+function printProblem(problem) {
+  console.log("Id:         ", problem.question.questionFrontendId);
+  console.log("Title:      ", problem.question.title);
+  console.log(
+    "Slug:       ",
+    problem.question.title
+      .split(" ")
+      .map((v) => capitalize(v))
+      .join(""),
+  );
+  console.log("Difficulty: ", problem.question.difficulty);
+}
+
+if (cache[todayKey]) {
+  printProblem(cache[todayKey]);
+  process.exit(0);
+}
+
 const _browser = chromium.use(stealth()).launch();
 const _page = _browser
   .then((browser) => browser.newPage())
@@ -33,17 +63,17 @@ const lc = new LeetCode();
 const daily = await lc.daily();
 await _browser.then((browser) => browser.close());
 
+// const tempFilePath = path.join(newTempDir, "example.txt");
+// fs.writeFileSync(tempFilePath, "This is a temporary file.");
+
 function capitalize(s) {
   return s[0].toUpperCase() + s.slice(1);
 }
 
-console.log("Id:         ", daily.question.questionFrontendId);
-console.log("Title:      ", daily.question.title);
-console.log(
-  "Slug:       ",
-  daily.question.title
-    .split(" ")
-    .map((v) => capitalize(v))
-    .join(""),
+cache[todayKey] = daily;
+await fs.writeFile(
+  path.join(leetCodeTempDir, "cache.json"),
+  JSON.stringify(cache),
 );
-console.log("Difficulty: ", daily.question.difficulty);
+
+printProblem(daily);
